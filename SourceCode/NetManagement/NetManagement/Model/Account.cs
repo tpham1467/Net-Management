7043ce86b0e858ae280a8dbd78d1d9ecd08ae573
Huy Pham <83506523+hphamp@users.noreply.github.com>
2022-05-27 16:01:27 +0700

    Hpham_27/05_16:01

--- SourceCode/NetManagement/NetManagement/Model/Account.cs
+++ SourceCode/NetManagement/NetManagement/Model/Account.cs
@@ -15,9 +15,9 @@ namespace NetManagement.Model
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int ID_Account { get; set; }
-         [Required]
+        [Required]
         public string UserName_Acc { get; set; }
-         [Required(ErrorMessage = "Mật khẩu không được để trống")]
+        [Required(ErrorMessage = "Mật khẩu không được để trống")]
         [RegularExpression("^[a-zA-Z0-9]*$", ErrorMessage = "Mật khẩu không chứa kí tự đặc biệt")]
         [StringLength(32, MinimumLength = 8, ErrorMessage = "Mật khẩu có độ dài từ 8 - 32")]
         public string Password_Acc { get; set; }
@@ -26,8 +26,8 @@ namespace NetManagement.Model
         public virtual Role Role { get; set; }
         [Required]
         public int Id_User {get; set;}
-     [ForeignKey("Id_User")]
-     public virtual User User {get; set;}
+        [ForeignKey("Id_User")]
+        public virtual User User {get; set;}
 
     }
 }
